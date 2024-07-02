8980462f0284fa6dc96730a31202ca45c306e073
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-07-02 20:15:37 +0500

    hero is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./assets/Pages/Header/Header";
+import Main from "./assets/Pages/Main/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main />
     </>
   );
 }
---
+++ src/assets/Pages/Hero/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Hero.css";
+const Hero = () => {
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="hero-wrapper">
+          <p className="hero-span">ELECTRICAL SOLUTIONS PROVIDER</p>
+          <h1 className="hero-title">Power Up Your Home with Doctor Energy</h1>
+          <button className="hero-btn">Contact Us Today</button>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
