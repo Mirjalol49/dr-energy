4bdc7e20c08a806251dfff5d86996c80081d3d60
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-07-16 19:04:28 +0500

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./assets/Pages/Header/Header";
 import Main from "./assets/Pages/Main/Main";
+import TestPage from "./assets/Pages/testpage/TestPage";
 
 function App() {
   return (
     <>
+      <TestPage />
       <Header />
       <Main />
     </>
---
+++ src/assets/Pages/testpage/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TestPage.css";
+const TestPage = () => {
+  return (
+    <div className="test">
+      Our website is currently under construction. For any inquiries, please
+      contact us at <span>+998 90 917 71 44.</span>
+    </div>
+  );
+};
+
+export default TestPage;
