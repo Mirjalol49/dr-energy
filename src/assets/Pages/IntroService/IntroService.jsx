fa6e2133ef6112b467517a90e84d7e003d3b450c
Mirjalol49 <mirjalolxurshida4941@gmail.com>
2024-08-13 17:13:22 +0500

    meta tag added

---
+++ src/assets/Pages/IntroService/IntroService.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./IntroService.css";
+const IntroService = () => {
+  return (
+    <div className="intro-service__wrapper">
+      <h3 className="intro-service__title">
+        At Electrician, we pride ourselves on our unwavering commitment to
+        quality. With a team of highly trained and certified electricians, we
+        are dedicated to delivering top-notchelectrical solutions for both
+        residential and commercial clients.
+      </h3>
+    </div>
+  );
+};
+
+export default IntroService;
--- src/assets/Pages/Main/Main.jsx
+++ src/assets/Pages/Main/Main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Hero from "../Hero/Hero";
+import IntroService from "../IntroService/IntroService";
 
 const Main = () => {
   return (
     <main>
       <Hero />
+      <IntroService />
     </main>
   );
 };
